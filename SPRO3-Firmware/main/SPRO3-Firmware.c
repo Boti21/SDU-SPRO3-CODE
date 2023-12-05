cf155d7ba41a38445764cbafe36434ac4280535e
n0hen <casper.hvide.bjerre@gmail.com>
2023-12-05 09:32:32 +0100

    update:ADC oneshot

--- SPRO3-Firmware/main/SPRO3-Firmware.c
+++ SPRO3-Firmware/main/SPRO3-Firmware.c
@@ -12,6 +12,7 @@
 #include "esp_log.h"
 #include "esp_adc_cal.h"
 #include "esp_err.h"
+#include "esp_adc/adc_oneshot.h" //New ADC libary
 
 
 /* Misc macros */
@@ -37,6 +38,10 @@ gptimer_handle_t timer = NULL;
 /* Prototypes */
 void init_adc(void);
 void init_ultrasonic(void);
+void init_adc_oneshot(void);
+
+/*Handles*/
+adc_oneshot_unit_handle_t adc1_handle;
 
 /*Global variables*/
 int inf_values[6];
@@ -79,6 +84,9 @@ void app_main(void)
     /* Initializing mutexes and semaphores */
     screen_mutex = xSemaphoreCreateMutex();
 
+    /*Init ADC*/
+    init_adc_oneshot();
+
     /* Little boot up message ;) */
     char *main_name = pcTaskGetName(NULL);     // A way to get the name of the current task
     ESP_LOGI(main_name, "Program started..."); // The wayto print something to the terminal
@@ -101,6 +109,10 @@ void app_main(void)
         ESP_LOGI(main_name, "this is a task");
         xSemaphoreGive(screen_mutex);
         */
+       //Code to be removed
+       int adc_value=0;
+       adc_oneshot_read(adc1_handle,0,&adc_value);
+       printf("ADC value: %d",adc_value);
     }
 }
 
@@ -119,6 +131,42 @@ void init_adc(void) {
     // Set pin GPO32 as an input
     gpio_num_t pinNumber = GPIO_NUM_32;
     gpio_set_direction(pinNumber, GPIO_MODE_INPUT);
+}
+
+void init_adc_oneshot(void){
+    //Creating oneshot handle
+    /*
+     * ADC configuration:
+     * unit: ADC 1
+     * ulp mode (Low power mode): Disable
+     * Clock: Default
+     */
+    adc_oneshot_unit_init_cfg_t init_config1={
+        .unit_id=ADC_UNIT_1,
+        .ulp_mode=ADC_ULP_MODE_DISABLE
+    };
+    //Check if handle allocation was successful
+    ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config1, &adc1_handle));
+
+    //Setting up ADC channels
+    /*
+    * ADC channel config:
+    * atten: Input voltage of ADC attenuated extending the range of measurement by about 11 dB
+    * bitwidth: default
+    */
+    adc_oneshot_chan_cfg_t config = {
+        .bitwidth = ADC_BITWIDTH_DEFAULT,
+        .atten = ADC_ATTEN_DB_11,
+    };
+    //Checking the channels
+    //Testing code:
+    ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, 0, &config));
+    //Actual code:
+    /*for (int i = 0; i < 8; i++)
+    {
+        ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle,i,&config));
+    }*/
+    
 }
 
 void init_ultrasonic(void) {
@@ -161,8 +209,7 @@ void infrared_adc_check(void){
     {
         for (int i = 0; i < 5; i++)
         {
-            int value = adc1_get_raw(PIN_INF+i);
-            inf_values[i]=value;
+            adc_oneshot_read(adc1_handle,0,&inf_values[i]);
         }
         
     }
