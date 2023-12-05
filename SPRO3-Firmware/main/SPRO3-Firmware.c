47dd89aee3229684143166207be69412cf7d433d
HenrikSDU <117083964+HenrikSDU@users.noreply.github.com>
2023-12-05 15:17:03 +0100

    Update SPRO3-Firmware.c

--- SPRO3-Firmware/main/SPRO3-Firmware.c
+++ SPRO3-Firmware/main/SPRO3-Firmware.c
@@ -41,6 +41,7 @@
 #define ADC2_3 GPIO_NUM_15
 
 #define IR_FRONT_NUMBER_OF_PINS 6
+#define IR_BACK_NUMBER_OF_PINS 2
 
 #define IR_FRONT_D1_GPIO ADC1_0
 #define IR_FRONT_D3_GPIO ADC1_3
@@ -77,9 +78,16 @@ void pwm_start();
 void pwm_stop();
 
 
+void infrared_adc_check_front(void);
+
+void infrared_adc_check_back(void);
+
 /*Global variables*/
-uint8_t IR_CHANNELS_FRONT[] = {IR_FRONT_D1_GPIO, IR_FRONT_D3_GPIO, IR_FRONT_D4_GPIO, IR_FRONT_D5_GPIO, IR_FRONT_D6_GPIO, IR_FRONT_D8_GPIO, IR_BACK_D4_GPIO, IR_BACK_D5_GPIO};
+uint8_t IR_CHANNELS_FRONT[] = {IR_FRONT_D1_GPIO, IR_FRONT_D3_GPIO, IR_FRONT_D4_GPIO, IR_FRONT_D5_GPIO, IR_FRONT_D6_GPIO, IR_FRONT_D8_GPIO};
 int inf_values_front[IR_FRONT_NUMBER_OF_PINS];
+
+uint8_t IR_CHANNELS_BACK[] = {IR_BACK_D4_GPIO, IR_BACK_D5_GPIO};
+int inf_values_back[IR_BACK_NUMBER_OF_PINS];
 
 void monitor_task(void* pvParameters) {
     for(;;) {
@@ -238,16 +246,28 @@ void init_ultrasonic(void)
 }
 
 /* Check each adc value of infrared sensor */
-void infrared_adc_check(void)
+void infrared_adc_check_front(void)
 {
     for (;;)
     {
         for (int i = 0; i < IR_FRONT_NUMBER_OF_PINS; i++)
         {
-            adc_oneshot_read(adc1_handle, IR_CHANNELS_FRONT[i], &inf_front_values[i]);
+            adc_oneshot_read(adc1_handle, IR_CHANNELS_FRONT[i], &inf_values_front[i]);
         }
     }
 }
+
+void infrared_adc_check_back(void)
+{
+    for (;;)
+    {
+        for (int i = 0; i < IR_FRONT_NUMBER_OF_PINS; i++)
+        {
+            adc_oneshot_read(adc1_handle, IR_CHANNELS_BACK[i], &inf_values_back[i]);
+        }
+    }
+}
+
 
 void init_pwm(int motor, int GPIO)
 {
