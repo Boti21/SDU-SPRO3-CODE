fc9f9a1baded747b857ce7d6b0f29fa105627331
Botond Bencsik <botibencsik@gmail.com>
2023-12-13 15:54:38 +0100

    monitor function

--- SPRO3-Firmware/main/FL_threads.h
+++ SPRO3-Firmware/main/FL_threads.h
@@ -37,14 +37,34 @@ TaskHandle_t test_handle = NULL;
 TaskHandle_t test_handle2 = NULL;
 TaskHandle_t monitor_handle = NULL;
 
+/* Variables */
+uint8_t ultrasonic_toggle = 0;
 
+
+// Functions which check for battery voltage and collision detection (and communication)
 void monitor_task(void* pvParameters) {
     for(;;) {
-        // Functions which check for battery voltage and collision detection (and communication)
+        // Endstops
         if(check_endstop_up() == 1) {
             // Endstop reached
+            pwm_stop(M_MOTOR);
         } else if(check_endstop_down() == 1) {
             // Endstop reached
+            pwm_stop(M_MOTOR);
+        }
+
+        // Ultrasonic
+        if(distance_ultrasonic() < 15 /* && Not turning right now or something, so the pallet doesn't trigger it */) {
+            // Distance to object less than 15 cm
+            pwm_stop(L_MOTOR);
+            pwm_stop(R_MOTOR);
+            ultrasonic_toggle = 1;
+        } else if(distance_ultrasonic() >= 15) {
+            // Not sure how to continue, mayba just go STRAIGHT but what if its in turn
+            // also possible to just not have recovery or do it via the website
+            if(ultrasonic_toggle == 1) {
+                pwm_drive(STRAIGHT);
+            }
         }
     }
 }
